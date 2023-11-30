e9a23fa250eedd4dad22f9e0dbc6c759b848bacc
JohnsL-U <uzun.cansel@gmail.com>
2023-12-01 01:45:50 +0300

    Updated to fix issue #12. Added progress bar, styling for results display.

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
+
 import langchain_helper as lch
 import streamlit as st
+import time
 
-image_path = '/Users/canseluzun/Desktop/Consent-Guardian/static/CG-Logo.png'
+image_path = '/Users/canseluzun/Desktop/Consent-Guardian/CG-Logo.png'
 st.image(image_path, width=300) 
 st.title("Your shield in the digital realm!")
 st.subheader("Where fairness meets privacy...")
+  
+action_type = st.sidebar.selectbox("What could Consent Guardian do to help you today?", ("Summarize", "On a scale of 1-10 Rate", "Answer Questions About"))
 
-uploaded_file = st.file_uploader("Upload your document", type=['txt', 'pdf', 'docx'])
-if uploaded_file is not None and st.button('Process'):
-    # To read file as string:
-    stringio = uploaded_file.read().decode("utf-8")
-    st.write("Document is being processed...")
-else:
-    st.warning("Please upload a document first.")
-
-action_type = st.sidebar.selectbox("What could Consent Guardian do to help you today?", ("Summarize", "Rate", "Answer Questions About"))
-
-if action_type == "Rate":
+if action_type == "On a scale of 1-10 Rate":
   user_input = st.sidebar.text_area(
-    label="What would you want your document to be rated on a scale of 1-10?",
+    label="What would you want your document to be rated for?",
     max_chars=30
     )
 
@@ -29,16 +23,30 @@ if action_type == "Answer Questions About":
     )
   
 if action_type == "Summarize":
-  user_input = st.sidebar.selectbox("Type of Summary?", ("Detailed", "Brief"))
+  user_input = st.sidebar.selectbox("Type of Summary?", ("Long", "Brief"))
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+  openai_api_key = st.text_input("OpenAI API Key", type="password")
+  "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
-if user_input:
-    if not openai_api_key:
+uploaded_file = st.file_uploader("Upload your document", type=['txt', 'pdf', 'docx'])
+if uploaded_file is not None:
+    document_content = uploaded_file.read().decode("utf-8")
+    if st.button('Process') and openai_api_key:
+        progress_bar = st.progress(0)
+        for percentage_completed in range(100):
+            time.sleep(0.05)
+            progress_bar.progress(percentage_completed + 1)
+        response = lch.process_document(action_type, user_input, openai_api_key, document_content)
+        st.success("Document processed successfully!")
+        st.markdown("### Analysis Results:")
+        st.text_area("Uploaded Document:", value=document_content, height=200)
+        st.text_area("Response:", value=response.get("response", ""), height=200)
+    else:
       st.info("Please add your OpenAI API key to continue.")
       st.stop()
-    response = lch.process_document(action_type, user_input, openai_api_key)
-    st.write(response['response'])
-    
+else:
+      st.warning("Please upload a document.")
+       
+
+
