0d2848f9a2ad37d83fde0e4f78be9c3fecaac70c
JohnsL-U <uzun.cansel@gmail.com>
2023-12-01 03:31:18 +0300

    Fixed the indentations for good coding practice.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import langchain_helper as lch
 import streamlit as st
 import time
@@ -31,22 +30,22 @@ with st.sidebar:
 
 uploaded_file = st.file_uploader("Upload your document", type=['txt', 'pdf', 'docx'])
 if uploaded_file is not None:
-    document_content = uploaded_file.read().decode("utf-8")
-    if st.button('Process') and openai_api_key:
-        progress_bar = st.progress(0)
-        for percentage_completed in range(100):
-            time.sleep(0.05)
-            progress_bar.progress(percentage_completed + 1)
-        response = lch.process_document(action_type, user_input, openai_api_key, document_content)
-        st.success("Document processed successfully!")
-        st.markdown("### Analysis Results:")
-        st.text_area("Uploaded Document:", value=document_content, height=200)
-        st.text_area("Response:", value=response.get("response", ""), height=200)
-    else:
-      st.info("Please add your OpenAI API key to continue.")
-      st.stop()
+  document_content = uploaded_file.read().decode("utf-8")
+  if st.button('Process') and openai_api_key:
+    progress_bar = st.progress(0)
+    for percentage_completed in range(100):
+      time.sleep(0.05)
+      progress_bar.progress(percentage_completed + 1)
+    response = lch.process_document(action_type, user_input, openai_api_key, document_content)
+    st.success("Document processed successfully!")
+    st.markdown("### Analysis Results:")
+    st.text_area("Uploaded Document:", value=document_content, height=200)
+    st.text_area("Response:", value=response.get("response", ""), height=200)
+  else:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
 else:
-      st.warning("Please upload a document.")
+  st.warning("Please upload a document.")
        
 
 
