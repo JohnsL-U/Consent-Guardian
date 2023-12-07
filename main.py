1cabc9c86a615694fa209a8d2b02e85ac37dfa76
JohnsL-U <uzun.cansel@gmail.com>
2023-12-07 03:47:08 +0300

    Added initial response session state and changed the feedback and reprocessing logic to accomodate sequential chains used.

--- main.py
+++ main.py
@@ -9,36 +9,23 @@ if 'processing' not in st.session_state:
   st.session_state.processing = False
 if 'feedback_given' not in st.session_state:
   st.session_state.feedback_given = False
+if 'initial_response' not in st.session_state:
+  st.session_state.initial_response = None
 
-image_path = '/Users/canseluzun/Desktop/Consent-Guardian/CG-Logo.png'
+image_path = 'https://private-user-images.githubusercontent.com/93824716/281902819-365092b7-f5ab-4807-ac91-043bda15d158.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDE5MTAwODUsIm5iZiI6MTcwMTkwOTc4NSwicGF0aCI6Ii85MzgyNDcxNi8yODE5MDI4MTktMzY1MDkyYjctZjVhYi00ODA3LWFjOTEtMDQzYmRhMTVkMTU4LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzEyMDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMxMjA3VDAwNDMwNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ0ODFiNzQ0NTQ1MmM0NzgyYjZkNjFiN2Q2ZDdkZDdhOWE4MjkxMGU2ZTQ2YmQ2OTJmYzBiMDFiYWUzOWNjZDImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.IS7dOH8ed-z1SNehga-9wuKCSiO2whqHR6yO7mosKyk'
 st.image(image_path, width=300) 
 st.title("Your shield in the digital realm!")
 st.subheader("Where fairness meets privacy...")
   
 action_type = st.sidebar.selectbox("What could Consent Guardian do to help you today?", ("Summarize", "On a scale of 1-10 Rate", "Answer Questions About"))
-
-if action_type == "On a scale of 1-10 Rate":
-  user_input = st.sidebar.text_area(
-    label="What would you want your document to be rated for?",
-    max_chars=30
-    )
-
-if action_type == "Answer Questions About":
-  user_input = st.sidebar.text_area(
-    label="What would you like to know about this document?",
-    max_chars=100
-    )
-  
-if action_type == "Summarize":
-  user_input = st.sidebar.selectbox("Type of Summary?", ("Long", "Brief"))
+user_input = st.sidebar.text_area("Enter your specific requirements:", max_chars=100)
 
 with st.sidebar:
   openai_api_key = st.text_input("OpenAI API Key", type="password")
   "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
-
+# Function to read different document types
 def read_pdf(file):
-  """Read and convert a PDF file to text."""
   reader = PyPDF2.PdfReader(file)
   text = ""
   for page in range(len(reader.pages)):
@@ -46,11 +33,11 @@ def read_pdf(file):
   return text
 
 def read_html(file):
-  """Read and convert an HTML file to text."""
   soup = BeautifulSoup(file, 'html.parser')
   return soup.get_text()
 
 uploaded_file = st.file_uploader("Upload your document", type=['txt', 'pdf', 'docx'])
+document_content = None
 if uploaded_file is not None:
   if uploaded_file.type == "application/pdf":
     document_content = read_pdf(uploaded_file)
@@ -59,38 +46,39 @@ if uploaded_file is not None:
   else:
     document_content = uploaded_file.read().decode("utf-8")
 
-  if st.button('Process') and openai_api_key:
-      st.session_state.processing = True
-      st.session_state.feedback_given = False
-
-  if st.session_state.processing and not st.session_state.feedback_given:
+# Process Button
+if st.button('Process') and openai_api_key and document_content:
+    st.session_state.processing = True
+    st.session_state.feedback_given = False
     progress_bar = st.progress(0)
     for percentage_completed in range(100):
         time.sleep(0.05)
         progress_bar.progress(percentage_completed + 1)
     response = lch.process_document(action_type, user_input, openai_api_key, document_content)
+    st.session_state.initial_response = response.get("response", "")
     st.success("Document processed successfully!")
     st.markdown("## Analysis Results")
     st.text_area("#### Uploaded Document:", value=document_content, height=200)
-    st.text_area("#### Response:", value=response.get("response", ""), height=200)
+    st.text_area("#### Response:", value=st.session_state.initial_response, height=200)
 
+# Feedback and Reprocessing Logic
+if st.session_state.processing and not st.session_state.feedback_given:
     st.markdown("#### Feedback")
     if st.button('👍'):
-      st.session_state.feedback_given = True
-      st.info("Thank you for your feedback!")
-    if st.button('👎'):
-      st.session_state.feedback_given = True
-      st.session_state.processing = False  
-      st.info("Please wait while we are improving your results!")
-      improved_response = lch.process_document(action_type, user_input, openai_api_key, document_content)
-      st.success("Document reprocessed successfully!")
-      st.markdown("#### Improved Analysis Results")
-      st.text_area("Improved Response:", value=improved_response.get("response", ""), height=200)
-  else:
-    st.info("Please add your OpenAI API key and a document to continue.")
-else:
-  st.warning("Please upload a document.")
+        st.session_state.feedback_given = True
+        st.info("Thank you for your feedback!")
+    elif st.button('👎'):
+        st.session_state.feedback_given = True
+        st.session_state.processing = False  
+        st.info("Please wait while we are improving your results!")
+        improved_response = lch.process_document(action_type, user_input, openai_api_key, document_content, feedback='negative', initial_response=st.session_state.initial_response)
+        st.success("Document reprocessed successfully!")
+        st.markdown("#### Improved Analysis Results")
+        st.text_area(value=improved_response.get("response", ""), height=200)
 
+# Additional UI Elements
+if not document_content:
+    st.warning("Please upload a document.")
+elif not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
 
-  
-  
