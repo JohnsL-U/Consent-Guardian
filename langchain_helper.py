2ce209afc76fd1f92a817f9d22e0450b3c4349d8
JohnsL-U <uzun.cansel@gmail.com>
2023-12-07 03:48:24 +0300

    Updated process document function. Added a second chain to refine or elaborate on initial response to improve user satisfaction.

--- langchain_helper.py
+++ langchain_helper.py
@@ -7,12 +7,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def process_document(action_type, user_input, openai_api_key, document):
+def process_document(action_type, user_input, openai_api_key, document, feedback=None, initial_response=None):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    prompt_template_name = PromptTemplate(
+    prompt_template_initial = PromptTemplate(
         input_variables = ['action_type','user_input'],
         template="I'm looking for assistance with a {document}. Please {action_type} it, considering the following aspect: {user_input}."
     )
-    chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="response" )
-    result = chain({'action_type': action_type, 'user_input': user_input, 'document':document})
+    chain_initial = LLMChain(llm=llm, prompt=prompt_template_initial, output_key="response" )
+
+     # If feedback is provided and it's negative, process the document further
+    if feedback and feedback == 'negative' and initial_response:
+        prompt_template_feedback = PromptTemplate(
+            input_variables=['response'],
+            template="The initial summary received negative feedback. Please refine or elaborate on this summary: {response}."
+        )
+        chain_feedback = LLMChain(llm=llm, prompt=prompt_template_feedback, output_key="refined_response")
+        result = chain_feedback({'response': initial_response})
+    else:
+        result = chain_initial({'action_type': action_type, 'user_input': user_input, 'document': document})
+
     return result
